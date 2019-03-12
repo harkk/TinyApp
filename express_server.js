4216b3dcb6d7918ec9725ca93a7228bff9d672bf
Harkamal Khabra <khabra07@gmail.com>
2019-03-12 22:29:39 +0000

    fixed add route for /urls

--- express_server.js
+++ express_server.js
@@ -34,11 +34,10 @@ app.get("/urls", (req, res) => {
 });
 
 // step 5. add second route and template
-app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  console.log(templateVars);
-  res.render("urls_show", templateVars);
-});
+// app.get("/urls/:shortURL", (req, res) => {
+//   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+//   res.render("urls_show", templateVars);
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
